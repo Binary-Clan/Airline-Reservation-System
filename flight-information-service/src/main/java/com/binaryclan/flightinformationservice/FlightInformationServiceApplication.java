29ae257943a5c652fb5abadc1a1a9344768e596b
Sudharaka <sudharakafernandobusiness@gmail.com>
2024-05-08 13:32:48 +0530

    Intial structure

---
+++ flight-information-service/src/main/java/com/binaryclan/flightinformationservice/FlightInformationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.binaryclan.flightinformationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInformationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightInformationServiceApplication.class, args);
+	}
+
+}
---
+++ flight-information-service/src/test/java/com/binaryclan/flightinformationservice/FlightInformationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binaryclan.flightinformationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightInformationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

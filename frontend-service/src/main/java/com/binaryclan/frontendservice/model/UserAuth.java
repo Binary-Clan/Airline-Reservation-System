1e85af3eb8e3f38f0cf1e9e3fd276160cb1507d6
rusirugunaratne <info.rusiru@gmail.com>
2024-05-20 13:34:51 +0530

    🔐 Spring Security Added

--- frontend-service/src/main/java/com/binaryclan/frontendservice/model/UserAuth.java
+++ frontend-service/src/main/java/com/binaryclan/frontendservice/model/UserAuth.java
@@ -1,22 +1,60 @@
 package com.binaryclan.frontendservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class UserAuth {
+@Builder
+public class UserAuth implements UserDetails {
 
     @Id // Marks id as the primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Specifies auto-generation strategy
     private int id;
     private String userName;
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
